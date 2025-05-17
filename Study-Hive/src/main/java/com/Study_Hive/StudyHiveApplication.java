acf4b6ace935e286301de1eda26c60f5097982b9
Harsh Vardhan Pandey <157698602+harshvp072@users.noreply.github.com>
2025-05-17 16:37:25 +0530

    initial commit

---
+++ Study-Hive/src/main/java/com/Study_Hive/StudyHiveApplication.java
@@ -0,0 +1,13 @@
+package com.Study_Hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHiveApplication.class, args);
+	}
+
+}
---
+++ Study-Hive/src/test/java/com/Study_Hive/StudyHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Study_Hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
